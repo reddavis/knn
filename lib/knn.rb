a64d33a3c5933ede65737751ca0a8bcf7733908b
Carlos Mateos <carlos.mateos@amayagaming.com>
2013-11-24 18:29:52 +0100

    Added block to use custom distance measurements

--- lib/knn.rb
+++ lib/knn.rb
@@ -1,12 +1,13 @@
 require 'distance_measures'
 
 class KNN
-  def initialize(data, options={})
+  def initialize(data, options={}, &block)
     @data = data
+    @custom_distance_measure = block
     @distance_measure = options[:distance_measure] || :euclidean_distance
   end
   
-  def nearest_neighbours(input, k=4)
+  def nearest_neighbours(input, k=4) 
     find_closest_data(input, k)
   end
   
@@ -15,14 +16,17 @@ class KNN
   def find_closest_data(input, k)
     begin
       calculated_distances = []
-    
       @data.each_with_index do |datum, index| #Ye olde english
-        distance = input.send(@distance_measure, datum)
+          distance = if @custom_distance_measure.nil?
+            input.send(@distance_measure, datum)
+          else
+            @custom_distance_measure.call(input, datum)
+          end
         calculated_distances << [index, distance, datum]
       end
     
       calculated_distances.sort {|x, y| x[1] <=> y[1]}.first(k)
-    rescue NoMethodError
+    rescue NoMethodError => e
       raise "Hey, that's not a measurement. Read the README for available measurements"
     end
   end  
